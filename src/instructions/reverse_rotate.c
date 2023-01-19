2257862fe094470b4163514dc6b0820994810922
astahjmo <contato.johnatan06@gmail.com>
2023-01-19 18:10:38 -0300

    feat: add reverse_rotate instruction

--- src/instructions/reverse_rotate.c
+++ src/instructions/reverse_rotate.c
@@ -1,13 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   reverse_rotate_a.c                                 :+:      :+:    :+:   */
+/*   reverse_rotate.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: johmatos <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/07 20:49:13 by johmatos          #+#    #+#             */
-/*   Updated: 2023/01/07 20:49:14 by johmatos         ###   ########.fr       */
+/*   Updated: 2023/01/19 18:05:24 by johmatos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "push_swap.h"
 
+void rrotate(t_stack *stack, char *instruction)
+{
+	t_node *temp;
+
+	temp = stack->head;
+	stack->head = stack->top;
+	stack->top = stack->top->back;
+	stack->top->next = NULL;
+	stack->head->next = temp;
+	temp->back = stack->head;
+	stack->head->back = NULL;
+	if (instruction)
+		ft_printf("rrr\n");
+	
+}
