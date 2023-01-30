ed18f27c2a272613280d7d5729c0be998654d2eb
astahjmo <you@example.com>
2023-01-30 18:56:07 -0300

    test: add main tests

--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: johmatos <johmatos@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: johmatos < johmatos@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/06 12:03:50 by johmatos          #+#    #+#             */
-/*   Updated: 2023/01/30 21:28:41 by johmatos         ###   ########.fr       */
+/*   Updated: 2023/01/30 18:40:30 by johmatos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,14 @@
 int	main(int argc, char *argv[])
 {
 	t_data	data;
+
 	if (!check_arguments(argc, argv))
-		return(ft_printf("Error \n"));
+		return (ft_printf("Error \n"));
 	data.stack_a = init_stack(argc, argv);
 	data.stack_b = init_stack(0, NULL);
 	printf_stack(data.stack_a->head);
-
+	push(data.stack_a, data.stack_b, PB);
+	push(data.stack_a, data.stack_b, PB);
 	push(data.stack_a, data.stack_b, PB);
 	push(data.stack_a, data.stack_b, PB);
 	push(data.stack_a, data.stack_b, PB);
