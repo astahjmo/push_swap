eaca1de1ed72f03c507dbc9f1e6e1be06f9213b5
astahjmo <you@example.com>
2023-02-01 16:49:43 -0300

    feat: improve print_ll tool

--- src/utils/print_ll.c
+++ src/utils/print_ll.c
@@ -3,20 +3,25 @@
 /*                                                        :::      ::::::::   */
 /*   print_ll.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: johmatos <johmatos@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: johmatos < johmatos@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/16 15:47:21 by johmatos          #+#    #+#             */
-/*   Updated: 2023/01/30 19:05:42 by johmatos         ###   ########.fr       */
+/*   Updated: 2023/01/31 18:37:35 by johmatos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	printf_stack(t_node *head)
+void	printf_stack(t_stack *stack, char id)
 {
 	t_node	*cursor;
 
-	cursor = head;
+	ft_printf("Stack info: %c\n", id);
+	ft_printf("Stack size: %d\n", stack->size);
+	ft_printf("Stack top: %d\n", stack->top->value);
+	ft_printf("Stack head: %d\n", stack->head->value);
+	ft_printf("Elements:\n", stack);
+	cursor = stack->head;
 	while (cursor)
 	{
 		ft_printf("%d\n", cursor->value);
