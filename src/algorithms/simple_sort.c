d8c5fb75579714ae77d20d66da96feaf765ee20d
astahjmo <you@example.com>
2023-02-03 16:37:18 -0300

    fix: simple sort backup

--- src/algorithms/simple_sort.c
+++ src/algorithms/simple_sort.c
@@ -6,53 +6,43 @@
 /*   By: johmatos < johmatos@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/31 19:06:39 by johmatos          #+#    #+#             */
-/*   Updated: 2023/02/02 21:47:32 by johmatos         ###   ########.fr       */
+/*   Updated: 2023/02/03 15:33:23 by johmatos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+void	solve_two(t_stack *a)
+{
+	if (a->top->value > a->top->back->value)
+		swap(a, SA);
+}
+
+static void	solve_three(t_stack *a, t_stack *b)
+{
+	int			top;
+	int			mid;
+	int			head;
+	static int	movs;
+
+	top = a->top->value;
+	head = a->head->value;
+	mid = a->head->next->value;
+	if ((head > top && head > mid)
+	 || (mid > head && movs < 1))
+		swap(a, SA);
+	else if (mid < head && mid < top)
+		rrotate(a, RRA);
+	else if (mid > top && mid > head)
+		rotate(a, RA);
+	movs++;
+}
+
 void	simple_sort(t_stack *a, t_stack *b)
 {
-	t_node	*carry;
-	int		aux;
-
-	while (!is_empty(a))
-	{
-		if  (is_empty(b))
-			push(a,b,PB);
-		carry = a->top;
-		if (carry->value > b->top->value)
-			push(a, b, PB);
-		else if (carry->value < b->top->value && b->top->back && carry->value > b->top->back->value)
-		{
-			push(a, b, PB);
-			swap(b,SB);
-		}
-		else
-		{
-			aux = carry->value;
-			pop(a);
-			while (!is_empty(b)) 
-			{
-				if (aux > b->top->value)
-				{
-					b->top = ft_add_back(ft_nodenew(aux), b->top);
-					b->size++;
-					break;
-				}
-				else if (aux < b->top->value && b->top->back && aux > b->top->back->value)
-				{
-					b->top = ft_add_back(ft_lastnode(b->head),
-					ft_nodenew(aux));
-					b->size++;
-					swap(b,SB);
-					break;
-				}
-				push(b, a,PA);
-			}
-		}
-	}
-	while (!is_empty(b))
-		push(b, a,PA);
+	if (a->size == 2)
+		solve_two(a);
+	if (a->size == 3)
+		while (!is_ordered(a))
+			solve_three(a, b);
 }
--- src/algorithms/sort_handler.c
+++ src/algorithms/sort_handler.c
@@ -6,7 +6,7 @@
 /*   By: johmatos < johmatos@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/31 16:24:23 by johmatos          #+#    #+#             */
-/*   Updated: 2023/02/02 19:18:48 by johmatos         ###   ########.fr       */
+/*   Updated: 2023/02/02 23:34:03 by johmatos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,7 @@
 
 void	sort_handler(t_stack *a, t_stack *b)
 {
-	if (a->size == 1)
-		return ;
-	if (a->size == 2)
-		if (a->head->value < a->top->value)
-			return (swap(a, SA));
-	if (is_empty(a))
-		return ;
-	if (!is_empty(b))
-		return ;
-	simple_sort(a, b);
+	if (a->size <= 3)
+		simple_sort(a, b);
 	return ;
 }
