5e530766984c4c4669f7c3cf4b995651821e8cca
astahjmo <you@example.com>
2023-02-05 12:49:43 -0300

    refactor: use is_empty function

--- src/logics/list_initialize.c
+++ src/logics/list_initialize.c
@@ -6,7 +6,7 @@
 /*   By: johmatos < johmatos@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/20 19:10:23 by johmatos          #+#    #+#             */
-/*   Updated: 2023/01/30 21:23:19 by johmatos         ###   ########.fr       */
+/*   Updated: 2023/02/05 12:19:48 by johmatos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,10 @@ static int	populate_stack(int size, char **childs, t_stack *stack)
 {
 	int		index;
 
-	index = size -1;
+	index = size - 1;
 	while (index >= 1)
 	{
-		if (stack->size == 0)
+		if (is_empty(stack))
 			stack->head = ft_nodenew(ft_atoi(childs[index]));
 		else
 			stack->top = ft_add_back(ft_lastnode(stack->head),
