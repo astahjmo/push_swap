95444c5dc689a3b5f1dc3e0c3c0afd718e947ce2
astahjmo <you@example.com>
2023-02-05 12:48:13 -0300

    fix: solve_three without loop

--- src/algorithms/simple_sort.c
+++ src/algorithms/simple_sort.c
@@ -6,7 +6,7 @@
 /*   By: johmatos < johmatos@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/31 19:06:39 by johmatos          #+#    #+#             */
-/*   Updated: 2023/02/04 19:32:12 by johmatos         ###   ########.fr       */
+/*   Updated: 2023/02/04 21:38:12 by johmatos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,23 +22,26 @@ void	solve_three(t_stack *a)
 {
 	int			top;
 	int			mid;
-	int			head;
+	int			bot;
 
 	top = a->top->value;
-	head = a->head->value;
+	bot = a->head->value;
 	mid = a->head->next->value;
-	if (mid < head && mid < top && head < top)
+	if (mid < bot && mid < top && bot < top)
 		rotate(a, RA);
-	else if (mid > top && mid > head && head < top)
+	else if (mid > top && mid > bot && bot < top)
 		rrotate(a, RRA);
 	else
 		swap(a, SA);
 }
 
-void	simple_sort(t_stack *a, t_stack *b)
+void	simple_sort(t_stack *a)
 {
 	if (a->size == 2)
 		solve_two(a);
 	if (a->size == 3)
-		solve_three(a);
+	{
+		while (!is_ordered(a))
+			solve_three(a);
+	}
 }
