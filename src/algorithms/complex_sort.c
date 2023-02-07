1c69350cc86a57292426454d4540757614d954d0
johmatos <contato.johnatan06@gmail.com>
2023-02-07 20:25:37 +0100

    add: complex sort with radix sort

--- src/algorithms/complex_sort.c
+++ src/algorithms/complex_sort.c
@@ -6,7 +6,7 @@
 /*   By: johmatos <johmatos@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/04 19:20:38 by johmatos          #+#    #+#             */
-/*   Updated: 2023/02/05 00:08:56 by johmatos         ###   ########.fr       */
+/*   Updated: 2023/02/07 19:56:03 by johmatos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,29 @@
 
 void	complex_sort(t_stack *a, t_stack *b)
 {
-	while (a->size != 3)
-		push(a, b, PB);
-	solve_three(a);
-	while (!is_empty(b))
-		dynamic_sort(a, b);
+	int	i;
+	int	j;
+	int	max_bits;
+	int	size;
+
+	i = 0;
+	size = a->size;
+	max_bits = 0;
+	while ((size >> max_bits))
+		max_bits++;
+	while (i < max_bits)
+	{
+		j = 0;
+		while (j < size)
+		{
+			if ((a->top->index >> i & 1) == 1)
+				rotate(a, RA);
+			else
+				push(a, b, PB);
+			j++;
+		}
+		while (!is_empty(b))
+			push(b, a, PA);
+		i++;
+	}
 }
