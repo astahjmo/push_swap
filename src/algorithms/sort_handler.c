ddcebb4bd59db740bd55b621fffb32c89ef62989
johmatos <contato.johnatan06@gmail.com>
2023-02-07 20:26:03 +0100

    fix: sort handler with less 5 elements

--- src/algorithms/sort_handler.c
+++ src/algorithms/sort_handler.c
@@ -6,7 +6,7 @@
 /*   By: johmatos < johmatos@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/31 16:24:23 by johmatos          #+#    #+#             */
-/*   Updated: 2023/02/04 19:32:23 by johmatos         ###   ########.fr       */
+/*   Updated: 2023/02/07 19:56:55 by johmatos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,11 @@
 
 void	sort_handler(t_stack *a, t_stack *b)
 {
-	if (a->size <= 3)
+	if (is_ordered(a))
+		return ;
+	if (a->size <= 5)
 		simple_sort(a, b);
 	else
-		complex_sort(a,b);
+		complex_sort(a, b);
 	return ;
 }
