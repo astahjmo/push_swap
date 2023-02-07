6fb631d2e12e9691be096591b833656b61b78a36
johmatos <contato.johnatan06@gmail.com>
2023-02-07 20:30:39 +0100

    fix: main flow of instructions

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: johmatos < johmatos@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/06 12:03:50 by johmatos          #+#    #+#             */
-/*   Updated: 2023/01/31 18:41:47 by johmatos         ###   ########.fr       */
+/*   Updated: 2023/02/07 20:30:19 by johmatos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,12 @@ int	main(int argc, char *argv[])
 	t_data	data;
 
 	if (!check_arguments(argc, argv))
-		return (ft_printf("Error \n"));
+		return (ft_printf("Error\n"));
 	data.stack_a = init_stack(argc, argv);
 	data.stack_b = init_stack(0, NULL);
-	// printf_stack(data.stack_a, 'a');
+	mapper_index(data.stack_a);
 	sort_handler(data.stack_a, data.stack_b);
+	clear_memory(data.stack_a);
+	clear_memory(data.stack_b);
+	return (0);
 }
