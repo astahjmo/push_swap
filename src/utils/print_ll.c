dd30348fe55f9edf76cceb2064e34532c1b346f7
johmatos <contato.johnatan06@gmail.com>
2023-02-13 15:26:29 +0100

    improve: output printf_stack

--- src/utils/print_ll.c
+++ src/utils/print_ll.c
@@ -6,7 +6,7 @@
 /*   By: johmatos <johmatos@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/11 19:40:27 by johmatos          #+#    #+#             */
-/*   Updated: 2023/02/11 19:40:29 by johmatos         ###   ########.fr       */
+/*   Updated: 2023/02/13 15:25:30 by johmatos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,10 @@ void	printf_stack(t_stack *stack, char id)
 	ft_printf("Stack head: %d\n", stack->head->value);
 	ft_printf("Elements:\n", stack);
 	cursor = stack->top;
+	ft_printf("Value\t-\tindex\n");
 	while (cursor)
 	{
-		ft_printf("%d - %d\n", cursor->value, cursor->index);
+		ft_printf("%d\t-\t%d\n", cursor->value, cursor->index);
 		cursor = cursor->back;
 	}
 }
